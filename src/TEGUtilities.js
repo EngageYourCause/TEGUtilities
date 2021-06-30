7db1a4cf55b4b5c2b8b680bdce3e27d36b43117b
Paul B. Joiner <paulj@engageyourcause.com>
2021-06-30 14:30:12 -0500

    Initial commit

---
+++ src/TEGUtilities.js
@@ -0,0 +1,197 @@
+class TEGUtilities {
+	constructor() {
+		let __instance = this;
+
+		__instance.semaphore = {
+
+		}
+
+		__instance.windowSize = {
+			// window size flags
+			isSmall  : false,
+			isMedium : false,
+			isLarge  : false,
+			isLarger : false,
+			isTall   : false,
+
+			// options for calculating the flags
+			options : {
+				smallMaxWidth  : 740,
+				mediumMaxWidth : 1024,
+				largeMaxWidth  : 2048,
+				tallMinHeight  : 820,
+				inclusive      : true, // should the window size comparisons include the break points?
+
+				// allow customizable callback
+				afterWindowSize : function() {
+
+					if (console) {
+						console.log('The default TEGUtilities.windowSize.afterWindowSize() method does nothing.');
+					} // end log
+					return false;
+				},
+			}, // end windowSize.options
+
+			/**
+			 * windowSize.update
+			 * @param {windowSize} windowSize
+			 */
+			update : function() {
+				if (windowSize.options.inclusive) {
+					windowSize.isSmall = window.innerWidth <= windowSize.options.smallMaxWidth;
+					windowSize.isMedium = TEGUtilities.betweenInclude(window.innerWidth, windowSize.options.smallMaxWidth, windowSize.options.mediumMaxWidth, TEGUtilities.BTWN_INCL_HIGHER);
+					windowSize.isLarge = TEGUtilities.betweenInclude(window.innerWidth, windowSize.options.mediumMaxWidth, windowSize.options.largeMaxWidth, TEGUtilities.BTWN_INCL_HIGHER);
+					windowSize.isTall = (window.innerHeight >= windowSize.options.tallMinHeight);
+				} else {
+					windowSize.isSmall = window.innerWidth < windowSize.options.smallMaxWidth;
+					windowSize.isMedium = TEGUtilities.betweenInclude(window.innerWidth, windowSize.options.smallMaxWidth, windowSize.options.mediumMaxWidth, TEGUtilities.BTWN_INCL_NONE);
+					windowSize.isLarge = TEGUtilities.betweenInclude(window.innerWidth, windowSize.options.mediumMaxWidth, windowSize.options.largeMaxWidth, TEGUtilities.BTWN_INCL_NONE);
+					windowSize.isTall = (window.innerHeight > windowSize.options.tallMinHeight);
+				}
+				windowSize.isLarger = window.innerWidth > windowSize.options.largeMaxWidth;
+				windowSize.options.afterWindowSize();
+			}, // end windowSize.update()
+
+			/**
+			 * windowSize.init
+			 * @param {Object} Options
+			 */
+			init : function(Options) {
+				this.options = {...this.options, ...Options};
+				window.windowSize = this;
+				window.windowSize.update();
+			}, // end windowSize.init()
+		}; // end windowSize
+	} // end constructor()
+
+	static get BTWN_INCL_NONE() { return 0; }
+
+	static get BTWN_INCL_LOWER() { return 1; }
+
+	static get BTWN_INCL_HIGHER() { return 2; }
+
+	static get BTWN_INCL_BOTH() { return 3; }
+
+	/**
+	 * between
+	 * Reurns TRUE if value is strictly between minimum and maximum.
+	 *
+	 * @param {number} value, number to test
+	 * @param {number} minimum, lowest point of acceptable range
+	 * @param {number} maximum, highest point of acceptable range
+	 * @returns {boolean} TRUE if value is between minimum and maximum
+	 */
+	static between(value, minimum, maximum) {
+		return TEGUtilities.betweenInclude(value, minimum, maximum, TEGUtilities.BTWN_INCL_NONE);
+	} // end between()
+
+	/**
+	 * betweenInclude
+	 * Reurns TRUE if value is between minimum and maximum allowing optional inclusion
+	 * of either or both limits.
+	 *
+	 * @param {number} value, number to test
+	 * @param {number} minimum, lowest point of acceptable range
+	 * @param {number} maximum, highest point of acceptable range
+	 * @param {number} inclusive, whether to include the end points as acceptable values
+	 * @returns {boolean} TRUE if value is between minimum and maximum
+	 */
+	static betweenInclude(value, minimum, maximum, inclusive) {
+		// bulletproof the arguments
+		const btwnValue   = value || 0,
+		      getMinimum  = minimum || 0,
+		      getMaximum  = maximum || 0,
+		      btwnMinimum = Math.min(getMinimum, getMaximum),
+		      btwnMaximum = Math.max(getMinimum, getMaximum);
+		let btwnInclusive = inclusive || TEGUtilities.BTWN_INCL_NONE;
+
+		// if inclusive, do inclusive comparison
+		switch (btwnInclusive) {
+			case TEGUtilities.BTWN_INCL_LOWER:
+				return btwnValue >= btwnMinimum &&
+				       btwnValue < btwnMaximum;
+
+			case TEGUtilities.BTWN_INCL_HIGHER:
+				return btwnValue > btwnMinimum &&
+				       btwnValue <= btwnMaximum;
+
+			case TEGUtilities.BTWN_INCL_BOTH:
+				return btwnValue >= btwnMinimum &&
+				       btwnValue <= btwnMaximum;
+
+			default:
+				return btwnValue > btwnMinimum &&
+				       btwnValue < btwnMaximum;
+		}
+	} // end between()
+
+	/**
+	 * debounce
+	 * Wait to run a function until a certain time has passed since the last call.
+	 *
+	 * @param {Function} originalFunction, original function to debounce
+	 * @param {Number} timeout, timeout in milliseconds for setTimout() call
+	 * @param {Array} otherArgs, an array of any other arguments needed by the function
+	 * @returns {Function} a wrapper around originalFunction that limits execution
+	 */
+	static debounce(originalFunction, timeout, otherArgs) {
+		let timer;
+		// make otherArgs optional
+		otherArgs = otherArgs || [];
+
+		return function() {
+			// ignore all calls until there aren't any for the specified period
+			clearTimeout(timer);
+			// set a new timeout for the specified delay
+			timer = setTimeout(function() {
+				// call the function in context and with additional arguments
+				originalFunction.apply(this, otherArgs);
+			}, timeout);
+		}; // return the function wrapped in a setTimeout structure
+	} // end debounce()
+
+	/**
+	 * throttle
+	 * Don't run a function more often than a certain period of time.
+	 *
+	 * @param {Function} originalFunction, original function
+	 * @param {Number} timeout, timeout in milliseconds for setTimout() call
+	 * @param {Array} [otherArgs], an array of any other arguments needed by the function
+	 * @returns {Function} a wrapper around originalFunction that limits execution
+	 */
+	static throttle(originalFunction, timeout, otherArgs) {
+		let needInvoke = true;
+		// make otherArgs optional
+		otherArgs = otherArgs || [];
+
+		return function() {
+			if (!needInvoke) return false;
+
+			// prevent next run
+			needInvoke = false;
+			// call the function in context and with additional arguments
+			originalFunction.apply(this, otherArgs);
+			// try again after the timeout
+			setTimeout(function() { needInvoke = true; }, timeout);
+		}; // end throttling wrapper
+	} // end throttle()
+
+	/**
+	 * preloadImages
+	 * Allow preload of images for faster update of display.
+	 *
+	 * @param {String[, String, ...]} arguments, one or more strings containing valid image URLs
+	 * @returns {Array} a jQuery object containing the preloaded images
+	 */
+	static preloadImages() {
+		let returnImages = ['placeholder'];
+
+		for (let offset in arguments) {
+			let newImage = document.createElement('img');
+			newImage.setAttribute('src', arguments[offset]);
+			returnImages.push(newImage);
+		}
+
+		return returnImages.slice(1);
+	}
+} // end TEGUtilities
---
+++ test/jasmine.spec.js
@@ -0,0 +1,189 @@
+describe('TEG Utilities', function() {
+	beforeAll(function() {
+		window.tu = new TEGUtilities();
+		window.tu.windowSize.init();
+		window.runCount = 0;
+		window.debounceCount = 0;
+		window.throttleCount = 0;
+		window.timer;
+
+		window.spiedUpon = {
+			debounced : TEGUtilities.debounce(function() {
+				                                  debounceCount++;
+				                                  if (window.runCount < 300) {
+					                                  window.timer = setTimeout(function() { spiedUpon.debounced(arguments); }, 200);
+					                                  runCount++;
+				                                  }
+			                                  },
+			                                  4000),
+			throttled : TEGUtilities.throttle(function() {
+				                                  throttleCount++;
+				                                  if (window.runCount < 300) {
+					                                  window.timer = setTimeout(function() { spiedUpon.throttled(arguments); }, 200);
+					                                  runCount++;
+				                                  }
+			                                  },
+			                                  4000),
+		}; // end spiedUpon
+	}); // end beforeAll()
+
+	describe('windowSize should detect the size of the viewable area', function() {
+		it(`should say isSmall is ${window.innerWidth <= 740}`, function() {
+			expect(windowSize.isSmall).toBe(window.innerWidth <= 740);
+		}); // end it('should say isSmall is ')
+
+		it(`should say isMedium is ${window.innerWidth > 740 &&
+		                             window.innerWidth <= 1024}`,
+		   function() {
+			   expect(windowSize.isMedium).toBe(window.innerWidth > 740 &&
+			                                       window.innerWidth <= 1024);
+		   }); // end it('should say isMedium is ')
+
+		it(`should say isLarge is ${window.innerWidth > 1024 &&
+		                            window.innerWidth <= 2048}`,
+		   function() {
+			   expect(windowSize.isLarge).toBe(window.innerWidth > 1024 &&
+			                                      window.innerWidth <= 2048);
+		   }); // end it('should say isLarge is ')
+
+		it(`should say isLarger is ${window.innerWidth > 2048}`,
+		   function() {
+			   expect(windowSize.isLarger).toBe(window.innerWidth > 2048);
+		   }); // end it('should say isLarge is ')
+
+		it(`should say isTall is ${window.innerHeight > 2048}`,
+		   function() {
+			   expect(windowSize.isTall).toBe(window.innerHeight >= 820);
+		   }); // end it('should say isLarge is ')
+	}); // end it('should detect window size')
+
+	it('between() should show number range inclusion', function() {
+		expect(TEGUtilities.between(0, 0, 0)).toBeFalse();
+		expect(TEGUtilities.between(1, 0, 2)).toBeTrue();
+	}); // end it('between should show number range inclusion')
+
+	describe('betweenInclude() should show number range inclusion', function() {
+		it('should allow inclusion of limits', function() {
+			expect(TEGUtilities.betweenInclude(0, 0, 0, TEGUtilities.BTWN_INCL_BOTH)).toBeTrue();
+		}); // end it('should allow inclusion of limits')
+
+		it('should should exclude values outside the limits', function() {
+			expect(TEGUtilities.betweenInclude(-2, 0, 2, TEGUtilities.BTWN_INCL_BOTH)).toBeFalse();
+		}); // end it('should should exclude values outside the limits')
+
+		it('should should include values between the limits', function() {
+			expect(TEGUtilities.betweenInclude(1, 0, 2)).toBeTrue();
+		}); // end it('should should include values between the limits')
+
+		it('should default to excluding the limits', function() {
+			expect(TEGUtilities.betweenInclude(2, 0, 2)).toBeFalse();
+		}); // end it('should default to excluding the limits')
+
+		it('should exclude the limits when explicitly told', function() {
+			expect(TEGUtilities.betweenInclude(2, 0, 2, TEGUtilities.BTWN_INCL_NONE)).toBeFalse();
+		}); // end it('should exclude the limits when explicitly told')
+
+		it('should include the higher limit when told', function() {
+			expect(TEGUtilities.betweenInclude(2, 0, 2, TEGUtilities.BTWN_INCL_HIGHER)).toBeTrue();
+		}); // end it('should include the higher limit when told')
+
+		it('should include the lower limit when told', function() {
+			expect(TEGUtilities.betweenInclude(0, 0, 2, TEGUtilities.BTWN_INCL_LOWER)).toBeTrue();
+		}); // end it('should include the lower limit when told')
+	}); // end it('between should show number range inclusion')
+
+	describe('debounce() and throttle()', function() {
+		beforeAll(function() {
+			jasmine.clock().install();
+			spyOn(spiedUpon, 'debounced').and.callThrough();
+			spyOn(spiedUpon, 'throttled').and.callThrough();
+		}); // end beforeEach()
+
+		describe('debounce()', function() {
+			it('should create a debounced function', function() {
+				window.runCount = 0;
+				clearTimeout(window.timer);
+				spiedUpon.debounced();
+				jasmine.clock().tick(5000);
+				expect(spiedUpon.debounced).toHaveBeenCalledTimes(2);
+				expect(window.debounceCount).toBe(1);
+			}); // end it('should create a debounced function')
+
+			it('should pass arguments', function() {
+				let count         = 0,
+				    results       = {},
+				    noSpyDebounce = TEGUtilities.debounce(function() {
+					                                          count += 1;
+					                                          arguments.count = count;
+					                                          results = arguments;
+				                                          },
+				                                          400,
+				                                          ['my list', 1, 'of arguments']);
+				noSpyDebounce();
+				jasmine.clock().tick(500);
+				expect(Object.keys(results).length).toBe(4);
+				expect(results.count).toBe(1);
+				expect(results['0']).toBe('my list');
+				expect(results['1']).toBe(1);
+				expect(results['2']).toBe('of arguments');
+			}); // end it('should pass arguments')
+		});
+
+		describe('throttle()', function() {
+			it('should create a throttled function', function() {
+				window.runCount = 0;
+				clearTimeout(window.timer);
+				spiedUpon.throttled();
+				jasmine.clock().tick(5000);
+				expect(spiedUpon.throttled).toHaveBeenCalledTimes(2);
+				expect(window.throttleCount).toBe(1);
+			}); // end it('should create a debounced function')
+
+			it('should pass arguments', function() {
+				let count         = 0,
+				    results       = {},
+				    noSpyThrottle = TEGUtilities.throttle(function() {
+					                                          results = {count: ++count, ...arguments};
+				                                          },
+				                                          400,
+				                                          ['my other list', 2, 'of arguments']);
+				noSpyThrottle();
+				expect(Object.keys(results).length).toBe(4);
+				expect(results.count).toBe(1);
+				expect(results['0']).toBe('my other list');
+				expect(results['1']).toBe(2);
+				expect(results['2']).toBe('of arguments');
+			}); // end it('should pass arguments')
+		});
+
+	}); // end it('should throttle a function')
+
+	describe('preloadImages()', function() {
+
+		it('should be preloaded', function() {
+			let isLoaded  = true,
+			    loadedSrc = [];
+
+			// run the preloader and check if all the elements in the returned array have a source in the list of images
+			TEGUtilities.preloadImages('preloadImages/carousel-defenders1.jpg', 'preloadImages/ff-20.jpg', 'preloadImages/hero-tscl-2.jpg')
+			      .forEach(function(element) {
+			      	let value = element.attributes.src.value;
+				      loadedSrc.push(value);
+
+				      if (!['preloadImages/carousel-defenders1.jpg', 'preloadImages/ff-20.jpg', 'preloadImages/hero-tscl-2.jpg'].includes(value)) {
+					      isLoaded = false;
+				      }
+			      });
+
+			// check if all members of the list of images are in the array taken from the generated img elements
+			['preloadImages/carousel-defenders1.jpg', 'preloadImages/ff-20.jpg', 'preloadImages/hero-tscl-2.jpg']
+				.forEach(function(currentSrc) {
+
+					if (!loadedSrc.includes(currentSrc)) {
+						isLoaded = false;
+					}
+				});
+			expect(isLoaded).toBeTrue();
+		});
+	}); // end describe('preloadImages()')
+});
